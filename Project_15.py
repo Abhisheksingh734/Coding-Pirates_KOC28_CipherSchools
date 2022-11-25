4de7a26b1675b6e7478ecd2fb6a348a6b6c6848a
Abhishek singh <114075050+Abhisheksingh734@users.noreply.github.com>
2022-11-25 21:36:02 +0530

    Project 15

--- Project_15.py
+++ Project_15.py
@@ -1,3 +1,16 @@
+#Project 15:
+# In this project you have to enter a positive integer range [A, B] and system will find out the status (Prime or composite) of each number available in the given range. At the end print the count also.
+# Note: Make use of efficient approach to check prime status of the number.
+# For example:
+# Range is (7,10) 
+# Then the status of each number in the range is:
+# 7 is prime
+# 8 is composite or not prime
+# 9 is composite
+# 10 is composite
+# Count: 1 prime and 3 composite numbers in the range.
+
+
 A=int(input("Enter the starting range: "))
 B=int(input("Enter the ending range: "))
 
